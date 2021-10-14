68b82247a8b89b01255d1616f3c306264cdf9f7d
carolina <caritonaranjo@gmail.com>
2021-10-13 22:05:11 -0500

    instalacion cors para permitir que se envie informacion de cualquier parte

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
+app.use(cors({
+    origin: '*'
+}));
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json
 app.use(bodyParser.json())
 
-app.post('/venta', (req, res) => {
+app.post('/roles', (req, res) => {
     console.log(req.body)
     res.send('Hello  prueba!');
 });
 
 app.listen(5000, () => {
-    console.log('Example app listening on port 3000!');
+    console.log('Example app listening on port 5000!');
 });
 
 //Run app, then load http://localhost:3000 in a browser to see the output.
