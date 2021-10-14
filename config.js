afc0c42a0e7f8d5689af1035eedc6106d3f210bb
Giovanni Romaña Cuesta <smithgiovanni53@gmail.com>
2021-10-13 21:28:20 -0500

    agregada conexion a la base de datos usando sequelize

---
+++ config.js
@@ -0,0 +1,9 @@
+// Informacion necesaria para establecer la conexion con la base de datos (credenciales)
+module.exports = {
+  database: {
+    username: 'root',
+    password: 'gio1234',
+    database: 'dynamic_hardware',
+    host: 'localhost',
+  },
+};
--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const app = express();
-
+const sequelize = require('./database/db.js');
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
- 
-// parse application/json
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
 
-app.post('/venta', (req, res) => {
-    console.log(req.body)
-    res.send('Hello  prueba!');
-});
+// parse application/json
+app.use(bodyParser.json());
+
+// app.post('/venta', (req, res) => {
+//     console.log(req.body)
+//     res.send('Hello  prueba!');
+// });
 
 app.listen(5000, () => {
-    console.log('Example app listening on port 3000!');
+  console.log('Example app listening on port 3000!');
+
+  // Conexion a la base de datos desde el servidor
+  sequelize
+    .sync({ force: false })
+    .then(() => {
+      console.log('Conexión a la base de datos exitosa');
+    })
+    .catch((error) => {
+      console.log('Error', error);
+    });
 });
 
 //Run app, then load http://localhost:3000 in a browser to see the output.
